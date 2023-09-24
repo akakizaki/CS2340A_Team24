5951fab027ae091ff3c1e6c01dcb332eda15288c
Josh Forden <26302898+air4den@users.noreply.github.com>
2023-09-24 19:28:40 -0400

    Added ending screen to Android Manifest XML

--- app/src/main/java/com/example/team24dungeoncrawler/EndingScreen.java
+++ app/src/main/java/com/example/team24dungeoncrawler/EndingScreen.java
@@ -3,9 +3,6 @@ package com.example.team24dungeoncrawler;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 
 public class EndingScreen extends AppCompatActivity {
 
@@ -16,8 +13,6 @@ public class EndingScreen extends AppCompatActivity {
         setContentView(R.layout.ending_screen);
 
         name = getIntent().getStringExtra("name");
-        //Testing to see if this prints
-        System.out.println("test: " + name);
 
     }
 }
