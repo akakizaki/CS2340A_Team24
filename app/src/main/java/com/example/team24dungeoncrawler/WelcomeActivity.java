10afa7175a1a9389e37a6cfc6afbc4eaee43f3d8
anjalik8204 <54959947+anjalik8204@users.noreply.github.com>
2023-09-24 09:42:42 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/team24dungeoncrawler/WelcomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.cs2340fr;
+import android.os.Bundle;
+import android.view.View;
+
+import android.app.Activity;
+
+public class WelcomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+    /*
+
+
+    public void startGame(View view) {
+        // Add code to start your game here, e.g., start a new activity.
+    }
+
+    public void exitGame(View view) {
+        // Add code to exit the game here.
+        finish(); // Close the WelcomeActivity.
+    }
+    */
+}
