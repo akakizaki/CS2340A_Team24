9bd7a119d3913e1c0a4556b24d101bc06138fcb9
Josh Forden <26302898+air4den@users.noreply.github.com>
2023-09-24 19:05:44 -0400

    Add Exit Game Button functionality to go to endScreen

--- app/src/main/java/com/example/team24dungeoncrawler/EndingScreen.java
+++ app/src/main/java/com/example/team24dungeoncrawler/EndingScreen.java
@@ -1,4 +1,23 @@
 package com.example.team24dungeoncrawler;
 
-public class EndingScreen {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class EndingScreen extends AppCompatActivity {
+
+    private String name;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ending_screen);
+
+        name = getIntent().getStringExtra("name");
+        //Testing to see if this prints
+        System.out.println("test: " + name);
+
+    }
 }
