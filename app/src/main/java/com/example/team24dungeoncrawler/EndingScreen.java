fba6b4741c11e6c961d9e9aef9fab6213ed2a5ea
rpatel77 <rpatel627@gatech.edu>
2023-09-24 20:29:03 -0400

    reverted EndingScreen.java to original

--- app/src/main/java/com/example/team24dungeoncrawler/EndingScreen.java
+++ app/src/main/java/com/example/team24dungeoncrawler/EndingScreen.java
@@ -1,20 +1,17 @@
 package com.example.team24dungeoncrawler;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EndingScreen extends AppCompatActivity {
 
     private String name;
-    private TextView textView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ending_screen);
 
         name = getIntent().getStringExtra("name");
-        //textView.setText("The End " + name);
     }
 }
