f03a6a847175ab32f6e4b47e836bc462c3526453
rpatel77 <rpatel627@gatech.edu>
2023-10-11 18:23:17 -0400

    junit test for player and leaderboard

---
+++ app/src/test/java/com/example/team24dungeoncrawler/LeaderboardTest.java
@@ -0,0 +1,29 @@
+package com.example.team24dungeoncrawler;
+import static org.junit.Assert.*;
+
+import com.example.team24dungeoncrawler.model.Attempt;
+import com.example.team24dungeoncrawler.model.LeaderBoard;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class LeaderboardTest {
+
+    @Test
+    public void testRecentAttempt() {
+        // Create a Leaderboard instance
+        LeaderBoard leaderboard = LeaderBoard.getInstance();
+
+        // Create test attempts
+        Attempt attempt1 = new Attempt("Player1", 100);
+        Attempt attempt2 = new Attempt("Player2", 200);
+
+        // Add attempts to the leaderboard
+        leaderboard.addAttempt(attempt1);
+        leaderboard.addAttempt(attempt2);
+
+        // Check if the attempts are added successfully
+        assertEquals("Player2", leaderboard.getRecentAttempt().getPlayerName());
+    }
+}
