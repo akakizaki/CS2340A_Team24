943615b415622aa08c4bf4c44f16e9183303dcf3
Joshua Forden <26302898+air4den@users.noreply.github.com>
2023-10-11 22:01:12 -0400

    Added 2 tests for player name and damageMultiplier (pt 2)

---
+++ app/src/test/java/com/example/team24dungeoncrawler/JoshFordenTests.java
@@ -0,0 +1,34 @@
+package com.example.team24dungeoncrawler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import com.example.team24dungeoncrawler.model.Player;
+
+public class JoshFordenTests {
+    /**
+     * Timeout var for tests. Consider altering based on your machine.
+     */
+    private static final int TIMEOUT = 200;
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void playerName() {
+        Player player1 = Player.getInstance("joshua!", "Easy");
+        assertEquals("joshua!", player1.getName());
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void playerDifficulty() {
+        Player player1 = Player.getInstance("joshua!", "Easy");
+        assertEquals(0.8, player1.getDamageMultiplier(), 0.1);
+    }
+
+
+
+}
