f6d5568613355e35f086ead5dbd0b069f24a4494
Pv10101 <pranavdhawale2015@gmail.com>
2023-10-22 23:48:50 -0400

    Tests

---
+++ app/src/test/java/com/example/team24dungeoncrawler/Sprint3TestUnit/Sprint3TestPD.java
@@ -0,0 +1,42 @@
+package com.example.team24dungeoncrawler.Sprint3TestUnit;
+
+import com.example.team24dungeoncrawler.model.Player;
+import com.example.team24dungeoncrawler.viewmodels.MainGameActivity;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.KeyEvent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+public class Sprint3TestPD {
+
+    private Player player;
+    MainGameActivity mga;
+    KeyEvent keyPressEvent = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_D);
+    @Before
+    public void playerConstructor() {
+        // Create a new Player instance before each test
+        player = Player.getInstance("TestPlayer", "Medium");
+
+    }
+
+    @Test
+    public void testplayerColision() {
+        assertEquals(player.getHealth(), 100);
+    }
+
+    @Test
+    public void testScreenSwitch() {
+        assertEquals(player.getSpeed(),0);
+    }
+
+}
