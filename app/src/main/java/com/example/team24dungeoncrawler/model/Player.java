a720188b79ba87d33175736b5d073074dc45d0d5
anjalik8204 <54959947+anjalik8204@users.noreply.github.com>
2023-11-12 17:49:01 -0500

    Update Player.java ( created a reset method)

--- app/src/main/java/com/example/team24dungeoncrawler/model/Player.java
+++ app/src/main/java/com/example/team24dungeoncrawler/model/Player.java
@@ -104,6 +104,34 @@ public class Player implements Observable {
 
 
     }
+    public void reset(String name, String difficulty) {
+        this.name = name;
+        this.direction = "";
+        this.speed = 0;
+        this.score = 0;
+        this.row = 3;
+        this.col = 1;
+
+        // Reset health and damageMultiplier based on the selected difficulty
+        switch (difficulty) {
+            case "Easy":
+                this.health = 150;
+                this.damageMultiplier = 0.8;
+                break;
+            case "Medium":
+                this.health = 100;
+                this.damageMultiplier = 1.0;
+                break;
+            case "Hard":
+                this.health = 50;
+                this.damageMultiplier = 1.2;
+                break;
+            default:
+                this.health = 100; // Default values for invalid difficulty
+                this.damageMultiplier = 1.0;
+                break;
+        }
+    }
     public void decreaseHealth() {
         // Decrease health based on the damageMultiplier
         this.health -= (int) (10 * damageMultiplier); // You can adjust the damage as needed
