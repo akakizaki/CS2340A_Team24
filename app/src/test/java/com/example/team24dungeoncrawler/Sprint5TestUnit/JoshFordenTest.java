78e3790c7c27494225bf63d0067826f3113c3068
Joshua Forden <26302898+air4den@users.noreply.github.com>
2023-11-25 15:13:30 -0600

    added ScoreUpdate JUnit Test

--- app/src/test/java/com/example/team24dungeoncrawler/Sprint5TestUnit/JoshFordenTest.java
+++ app/src/test/java/com/example/team24dungeoncrawler/Sprint5TestUnit/JoshFordenTest.java
@@ -2,16 +2,98 @@ package com.example.team24dungeoncrawler.Sprint5TestUnit;
 
 import static org.junit.Assert.assertEquals;
 
+import android.view.KeyEvent;
+import android.widget.GridLayout;
+
+import com.example.team24dungeoncrawler.R;
+import com.example.team24dungeoncrawler.model.ExitStrategy;
+import com.example.team24dungeoncrawler.model.MoveRightStrategy;
+import com.example.team24dungeoncrawler.model.MovementStrategy;
 import com.example.team24dungeoncrawler.model.Player;
+import com.example.team24dungeoncrawler.model.PlayerView;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
 public class JoshFordenTest {
 
     private Player player;
+    private long visibleStartTime;
+    private int currentScore;
+    private int[][] tilemap;
+    private PlayerView playerView;
     @Before
     public void setUp() {
         // Initialize a Player instance before each test
-        player = Player.getInstance("TestPlayer", "Medium");
+        player = Player.getInstance("TestPlayer1", "Medium");
+        currentScore = 10;
+        //playerView = new PlayerView(JoshFordenTest context); // Create a new PlayerView
+        tilemap = new int[][]{
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5}
+        };
     }
+
+    @Test
+    public void playerInitPosition() {
+        assertEquals(player.getCol(), 1);
+        assertEquals(player.getRow(), 3);
+        assertEquals(currentScore, 11);
+    }
+
+    @Test
+    public void wait_TooLong_BeforeReachingDoor() throws InterruptedException {
+        visibleStartTime = System.currentTimeMillis();
+        Thread.sleep(10000);
+        player.setCol(19);
+        player.setRow(3);
+
+        playerMoving();
+
+        assertEquals(currentScore, 10);
+    }
+    
+
+
+
+    private void playerMoving() {
+
+        MovementStrategy movementStrategy = new MoveRightStrategy();
+
+        if (movementStrategy != null) {
+            int newTileType = tilemap[player.getRow()][player.getCol()];
+            if (newTileType == 3) {
+
+                //Player gets 10 - (seconds to reach the door) points after reaching door
+                long timeToReachDoor = System.currentTimeMillis() - visibleStartTime;
+                System.out.println(timeToReachDoor);
+                int scoreChange = (10 - (int) (timeToReachDoor / 1000)) * 10;
+                if (scoreChange <= 0) {
+                    scoreChange = 1;
+                }
+                currentScore += scoreChange;
+            }
+        }
+    }
+
 }
