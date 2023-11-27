dcf6e935cd182d55925ea345f13e511f021cb5a1
jadacrockett <143543725+jadacrockett@users.noreply.github.com>
2023-11-27 15:44:21 -0500

    Revert "Created Basis for Decorator Pattern"
    This reverts commit f047d62b19f0d7d20e41e34b080c769fbf4af425.
    
    Reverts basis of decorator pattern

--- app/src/main/java/com/example/team24dungeoncrawler/model/Player.java
+++ app/src/main/java/com/example/team24dungeoncrawler/model/Player.java
@@ -172,5 +172,6 @@ public class Player implements Observable, PlayerInterface {
 
     @Override
     public void update(Player player){}
+
 }
 
---
+++ app/src/test/java/com/example/team24dungeoncrawler/Sprint5TestUnit/jcrockettTest.java
@@ -0,0 +1,57 @@
+package com.example.team24dungeoncrawler.Sprint5TestUnit;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.team24dungeoncrawler.model.Enemy;
+import com.example.team24dungeoncrawler.model.EnemyFactory;
+import com.example.team24dungeoncrawler.model.HealthDecorator;
+import com.example.team24dungeoncrawler.model.Player;
+import com.example.team24dungeoncrawler.model.PlayerInterface;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class jcrockettTest {
+    private PlayerInterface player;
+
+    private int[][] tilemap;
+
+    @Before
+    public void setUp() {
+        player = Player.getInstance("John", "Easy");
+        tilemap = new int[][]{
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4},
+                {0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5}
+        };
+    }
+
+    @Test
+    public void testHealthDecorator() {
+        Player playerCast = (Player) player;
+        assertEquals(150, playerCast.getHealth());
+        //player = new HealthDecorator(player);
+        player = new HealthDecorator(player);
+        playerCast = (Player) player;
+        assertEquals(175, playerCast.getHealth());
+    }
+
+
+}
