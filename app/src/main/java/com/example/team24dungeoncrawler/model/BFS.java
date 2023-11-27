4f719d454ba3e575120342804a1a553dc2b25a78
Pv10101 <pranavdhawale2015@gmail.com>
2023-11-26 22:25:10 -0500

    BFS plus checkstyle

--- app/src/main/java/com/example/team24dungeoncrawler/model/BFS.java
+++ app/src/main/java/com/example/team24dungeoncrawler/model/BFS.java
@@ -4,13 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BFS {
-    static class Point {
-        int x,y;
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+
     static boolean isValid(int[][] map, boolean[][] visited, int x, int y) {
         int rows = map.length;
         int cols = map[1].length;
@@ -20,8 +14,8 @@ public class BFS {
     }
 
     static void algo(int[][] map, int begx, int begy) {
-        int[] xV = { -1, 0, 0, 1 };
-        int[] yV = { 0, -1, 1, 0 };
+        int[] xV = {-1, 0, 0, 1 };
+        int[] yV = {0, -1, 1, 0 };
 
         int rows = map.length;
         int cols = map[0].length;
@@ -29,21 +23,41 @@ public class BFS {
 
         Queue<Point> q = new LinkedList<>();
         visited[begx][begy] = true;
-        q.add(new Point(begx,begy));
+        q.add(new Point(begx, begy));
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             Point curr =  q.poll();
             int x = curr.x;
             int y = curr.y;
-            for(int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++) {
                 int nexX = x + xV[i];
                 int nexY = y + yV[i];
 
-                if(isValid(map, visited, nexX, nexY)) {
+                if (isValid(map, visited, nexX, nexY)) {
                     visited[nexX][nexY] = true;
                     q.add(new Point(nexX, nexY));
                 }
             }
+        }
+    }
+    static class Point {
+        private int x;
+        private int y;
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public int getX() {
+            return x;
+        }
+        public int getY() {
+            return y;
+        }
+        public void setX(int x) {
+            this.x = x;
+        }
+        public void setY(int y) {
+            this.y = y;
         }
     }
 }
