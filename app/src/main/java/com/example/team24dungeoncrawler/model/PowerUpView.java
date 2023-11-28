3db1d70b47a3a7694645a1a7d85675279b864e6d
jadacrockett <143543725+jadacrockett@users.noreply.github.com>
2023-11-28 09:33:06 -0500

    Created view for powerups

--- app/src/main/java/com/example/team24dungeoncrawler/model/PowerUpView.java
+++ app/src/main/java/com/example/team24dungeoncrawler/model/PowerUpView.java
@@ -1,2 +1,21 @@
-package com.example.team24dungeoncrawler.model;public class PowerUpView {
+package com.example.team24dungeoncrawler.model;
+
+import android.content.Context;
+import android.widget.GridLayout;
+
+public class PowerUpView extends androidx.appcompat.widget.AppCompatImageView {
+
+    public PowerUpView(Context context) {
+        super(context);
+    }
+
+    public void updatePosition(int row, int col) {
+        // Set the position of the enemy view on the grid
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+        params.rowSpec = GridLayout.spec(row);
+        params.columnSpec = GridLayout.spec(col);
+        setLayoutParams(params);
+        invalidate();
+    }
+
 }
